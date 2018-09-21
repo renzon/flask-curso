9874c69daff55f841d746292d20dc53b255fe11c
renzon <renzon@gmail.com>
2018-09-21 11:52:10 -0300

    Exemplo de teste simples com flask

--- test_hello.py
+++ test_hello.py
@@ -1,2 +1,13 @@
-def test_hello():
-    assert 1 == 0
+from app import app
+
+
+def test_hello_status_code():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_hello_msg():
+    client = app.test_client()
+    response = client.get('/')
+    assert 'Olá Mundo' in response.get_data(as_text=True)
