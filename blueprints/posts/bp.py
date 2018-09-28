5c977a1d60ce581f9b7b84e64526a9aef8a5ea67
renzon <renzon@gmail.com>
2018-09-28 15:18:49 -0300

    Implementado template com CRUD tosco de modelo

--- blueprints/posts/bp.py
+++ blueprints/posts/bp.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 from blueprints.posts.models import Post
 from ext.db import db
 
-bp = Blueprint('posts', __name__)
+bp = Blueprint('posts', __name__, template_folder='templates')
 
 
 def init_app(app, url_prefix='/posts'):
@@ -12,7 +12,17 @@ def init_app(app, url_prefix='/posts'):
 
 @bp.route('/new', methods=['GET', 'POST'])
 def new():
-    post = Post(title='Meu Post', content='Meu conteúdo')
+    if request.method == 'GET':
+        return render_template('posts/form.html')
+    form = request.form
+    post = Post(title=form['title'], content=form['content'])
     db.session.add(post)
     db.session.commit()
     return repr(post)
+
+
+@bp.route('/', )
+def list_posts():
+    query = Post.query.order_by(Post.title.asc())
+    posts = query[:10]
+    return render_template('posts/index.html', posts=posts)
