f752ba4c0caa064a659b173ecdc91b7110e9f603
renzon <renzon@gmail.com>
2018-09-28 15:31:34 -0300

    Instalada Flask Debug Tool Bar

--- app_factory.py
+++ app_factory.py
@@ -3,7 +3,7 @@ from flask import Flask
 import settings
 from blueprints.names.bp import bp as names_bp
 from blueprints.posts.bp import init_app as posts_init_app
-from ext import db
+from ext import db, debugtoolbar
 
 
 def create_app(default_settings=None):
@@ -13,6 +13,7 @@ def create_app(default_settings=None):
     app.config.from_object(default_settings)
     app.debug = app.config['DEBUG']
     db.init_app(app)
+    debugtoolbar.init_app(app)
     app.register_blueprint(names_bp, url_prefix='/')
     posts_init_app(app)
     return app
--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ from decouple import config
 DEBUG = config('DEBUG', default=False, cast=bool)
 SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
